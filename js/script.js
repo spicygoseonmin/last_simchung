64f699cf43a64d05cb5bd4ebe22388d152dc4900
seonmin <seonmin1414@gmail.com>
2024-07-16 14:48:54 +0900

    2024. 07. 16 (화)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+// 헤더 메뉴 (생물) 리스트 호버
 $(document).ready(function(){
-    $(".header-menu > .wrap > ul > li").mouseover(function(){
+    $(".header-menu-wrap > ul > li").mouseover(function(){
         // console.log(this);
         $(this).find(".submenu").stop().slideDown(200);
     }).mouseout(function(){
@@ -7,6 +8,15 @@ $(document).ready(function(){
     });
 });
 
+// 헤더 메뉴 (더보기) 리스트 호버
+$(document).ready(function(){
+    $(".addMenubar").mouseover(function(){
+        // console.log(this);
+        $(this).find(".submenu").stop().slideDown(200);
+    }).mouseout(function(){
+        $(this).find(".submenu").stop().slideUp(200);
+    });
+});
 //  검색창 클릭
 // const searchIcon = document.getElementById("search-icon");
 // const searchHover = document.getElementById("searchWindow");
@@ -21,5 +31,31 @@ window.onload = function () {
     searchIcon.onclick = function(){
         searchHover.classList.toggle("openSearchWindow");
     }
+
+    const swPop = new Swiper(".popular_list", {
+        slidesPerView: 5,
+        grid: {
+            rows: 1,
+            fill: "row",
+            },
+        spaceBetween: 0,
+        breakpoints: {
+            480: {
+            slidesPerView: 2,
+            },
+            760: {
+            slidesPerView: 2,
+            },
+        },
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+      });
 };
 
